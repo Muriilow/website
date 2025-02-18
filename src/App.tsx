cad6112b0222a48c55e0614b46a14f3581be2944
Muriilo <87148235+Muriilo779@users.noreply.github.com>
2025-02-18 15:07:15 -0300

    Creating alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
+import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 
 function App()
 {
+  //Alert 
+  const [alertVisible, setAlertVisibility] = useState(false);
   //List
   let itemsList = ["Capybara", "Bomberman", "SpaceShooter", "Pingo"];
   let header = "Games";
   const handleSelection = (item: string) => {}
 
+
   //Button
   let buttonType = "dark";
-  const onClick = () => 
-  {
-
-  };
 
   return (
     <>
+      { alertVisible && <Alert onClick={() => setAlertVisibility(false)}> My alert </Alert> }
+
       <div><ListGroup items={itemsList} heading={header} onSelectItem={handleSelection} /></div>
 
       <div><Button onClick={() => console.log('Clicked')} classType={buttonType}>Comprar</Button></div>
-      <div><Button onClick={onClick}>Baixar</Button></div>
+      <Button onClick={() => setAlertVisibility(true)}>Baixar</Button>
     </>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,15 +1,28 @@
+import { ReactNode } from "react";
 interface AlertProps 
 {
-
+  children: ReactNode;
+  onClick: () => null; 
 }
 
-function Alert() {
-  return 
-  (
-    <>
-      <div class = "alert alert-warning alert-dismissible fade show"> </div>
-    </>
-  );  
+function Alert({ children, onClick }: AlertProps)
+{
+  return (
+      <div 
+      className="alert alert-warning alert-dismissible fade show" 
+      role="alert"
+      >
+
+        {children}
+      
+        <button 
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClick}/>
+      </div>
+  );
 }
 
 export default Alert;
